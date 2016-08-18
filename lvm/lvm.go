349a75953912292cc1edf6be6e8835b52af860a4
Szymon Pyżalski <spyzalski@mirantis.com>
2016-08-18 10:09:21 +0200

    gofmted

--- lvm/lvm.go
+++ lvm/lvm.go
@@ -16,12 +16,12 @@ var SizeRE = regexp.MustCompile(
 )
 
 type AttachOpts struct {
-    FsType string `json:"kubernetes.io/fsType"`
-    Readwrite string `json:"kubernetes.io/readwrite"`
-    RawSize string `json:"size"`
-    VolumeID string `json:"volumeID"`
-    VolumeGroup string `json:"volumegroup"`
-    Pool string `json:"pool"`
+	FsType      string `json:"kubernetes.io/fsType"`
+	Readwrite   string `json:"kubernetes.io/readwrite"`
+	RawSize     string `json:"size"`
+	VolumeID    string `json:"volumeID"`
+	VolumeGroup string `json:"volumegroup"`
+	Pool        string `json:"pool"`
 }
 
 func (opts AttachOpts) Size() (uint64, error) {
@@ -31,104 +31,104 @@ func (opts AttachOpts) Size() (uint64, error) {
 	}
 	number, _ := strconv.ParseUint(groups[1], 10, 64) // Already validated by re
 	switch strings.ToLower(groups[2]) {
-		case "", "k":
-			return number * (1 << 10), nil
-		case "m":
-			return number * (1 << 20), nil
-		case "g":
-			return number * (1 << 30), nil
-		case "t":
-			return number * (1 << 40), nil
-		case "p":
-			return number * (1 << 50), nil
-        default:
-            panic("Unreachable!")
+	case "", "k":
+		return number * (1 << 10), nil
+	case "m":
+		return number * (1 << 20), nil
+	case "g":
+		return number * (1 << 30), nil
+	case "t":
+		return number * (1 << 40), nil
+	case "p":
+		return number * (1 << 50), nil
+	default:
+		panic("Unreachable!")
 	}
 }
 
 func PrintResult(status string, message string, device string) {
-    data := map[string]string{
-        "status": status,
-        "message": message,
-    }
-    if device != "" {
-        data["device"] = device
-    }
-    result, _ := json.Marshal(data)
-    fmt.Printf("%s\n", result)
+	data := map[string]string{
+		"status":  status,
+		"message": message,
+	}
+	if device != "" {
+		data["device"] = device
+	}
+	result, _ := json.Marshal(data)
+	fmt.Printf("%s\n", result)
 }
 
 func Init() {
-    PrintResult("Success", "No initialisation logic needed", "")
+	PrintResult("Success", "No initialisation logic needed", "")
 }
 
 func Attach(jsonArgStr string) {
-    var jsonArgs AttachOpts
-    _ = json.Unmarshal([]byte(jsonArgStr), &jsonArgs)
-    size, err := jsonArgs.Size()
-    if err != nil {
-        PrintResult("Failure", err.Error(), "")
-    }
-    device, err, created := lvm_utils.EnsureDevice(
+	var jsonArgs AttachOpts
+	_ = json.Unmarshal([]byte(jsonArgStr), &jsonArgs)
+	size, err := jsonArgs.Size()
+	if err != nil {
+		PrintResult("Failure", err.Error(), "")
+	}
+	device, err, created := lvm_utils.EnsureDevice(
 		jsonArgs.VolumeGroup,
 		jsonArgs.Pool,
 		jsonArgs.VolumeID,
 		size,
-        jsonArgs.FsType,
+		jsonArgs.FsType,
 	)
-    if err != nil {
-        PrintResult("Failure", err.Error(), "")
-        return
-    }
-    message := "Volume already exists"
-    if created {
-        message = "Volume created"
-    }
-    PrintResult("Success", message, device.Path())
+	if err != nil {
+		PrintResult("Failure", err.Error(), "")
+		return
+	}
+	message := "Volume already exists"
+	if created {
+		message = "Volume created"
+	}
+	PrintResult("Success", message, device.Path())
 }
 
 func Mount(target string, device string, jsonArgStr string) {
-    var jsonArgs AttachOpts
-    _ = json.Unmarshal([]byte(jsonArgStr), &jsonArgs)
-    err := os.MkdirAll(target, 0700)
-    if err != nil {
-        PrintResult("Failure", err.Error(), "")
-        return
-    }
-    err = syscall.Mount(device, target, jsonArgs.FsType, 0, "")
-    if err != nil {
-        PrintResult("Failure", err.Error(), "")
-        return
-    }
-    PrintResult("Success", "Volume mounted", "")
+	var jsonArgs AttachOpts
+	_ = json.Unmarshal([]byte(jsonArgStr), &jsonArgs)
+	err := os.MkdirAll(target, 0700)
+	if err != nil {
+		PrintResult("Failure", err.Error(), "")
+		return
+	}
+	err = syscall.Mount(device, target, jsonArgs.FsType, 0, "")
+	if err != nil {
+		PrintResult("Failure", err.Error(), "")
+		return
+	}
+	PrintResult("Success", "Volume mounted", "")
 }
 
 func Detach() {
-    PrintResult("Success", "No detachment logic needed", "")
+	PrintResult("Success", "No detachment logic needed", "")
 }
 
 func Unmount(path string) {
-    err := syscall.Unmount(path, 0)
-    if err != nil {
-        PrintResult("Failure", err.Error(), "")
-    }
-    PrintResult("Success", "Volume unmounted", "")
+	err := syscall.Unmount(path, 0)
+	if err != nil {
+		PrintResult("Failure", err.Error(), "")
+	}
+	PrintResult("Success", "Volume unmounted", "")
 
 }
 
 func main() {
-    switch os.Args[1] {
-    case "init":
-        Init()
-    case "attach":
-        Attach(os.Args[2])
-    case "detach":
-        Detach()
-    case "mount":
-        Mount(os.Args[2], os.Args[3], os.Args[4])
-    case "unmount":
-        Unmount(os.Args[2])
-    default:
-        PrintResult("Failure", "Invalid command", "")
-    }
+	switch os.Args[1] {
+	case "init":
+		Init()
+	case "attach":
+		Attach(os.Args[2])
+	case "detach":
+		Detach()
+	case "mount":
+		Mount(os.Args[2], os.Args[3], os.Args[4])
+	case "unmount":
+		Unmount(os.Args[2])
+	default:
+		PrintResult("Failure", "Invalid command", "")
+	}
 }
--- lvm_utils/lvm_utils.go
+++ lvm_utils/lvm_utils.go
@@ -15,105 +15,98 @@ import "unsafe"
 var libHandle C.lvm_t
 
 type Lv struct {
-    lv C.lv_t
+	lv C.lv_t
 }
-
 
 func init() {
-    path := C.CString("etc/lvm")
-    defer C.free(unsafe.Pointer(path))
-    libHandle = C.lvm_init(path)
+	path := C.CString("etc/lvm")
+	defer C.free(unsafe.Pointer(path))
+	libHandle = C.lvm_init(path)
 }
-
 
 func GetVG(vgName string) (C.vg_t, error) {
-    goVgName := C.CString(vgName)
-    defer C.free(unsafe.Pointer(goVgName))
-    w := C.CString("w")
-    defer C.free(unsafe.Pointer(w))
-    vg := C.lvm_vg_open(libHandle, goVgName, w, 0)
-    if (vg == nil) {
-	    return nil, errors.New(C.GoString(C.lvm_errmsg(libHandle)))
-    } else {
-        return vg, nil
-    }
+	goVgName := C.CString(vgName)
+	defer C.free(unsafe.Pointer(goVgName))
+	w := C.CString("w")
+	defer C.free(unsafe.Pointer(w))
+	vg := C.lvm_vg_open(libHandle, goVgName, w, 0)
+	if vg == nil {
+		return nil, errors.New(C.GoString(C.lvm_errmsg(libHandle)))
+	} else {
+		return vg, nil
+	}
 }
-
 
 func GetLV(vg C.vg_t, lvName string) (*Lv, error) {
-    goLvName := C.CString(lvName)
-    defer C.free(unsafe.Pointer(goLvName))
-    lv := C.lvm_lv_from_name(vg, goLvName);
-    if (lv == nil) {
-        message := C.GoString(C.lvm_errmsg(libHandle))
-        if message == "" {
-            return nil, nil // LV doesn't exist
-        } else {
-            return nil, errors.New(message) // Some error
-        }
-    }
-    return &Lv{lv}, nil
+	goLvName := C.CString(lvName)
+	defer C.free(unsafe.Pointer(goLvName))
+	lv := C.lvm_lv_from_name(vg, goLvName)
+	if lv == nil {
+		message := C.GoString(C.lvm_errmsg(libHandle))
+		if message == "" {
+			return nil, nil // LV doesn't exist
+		} else {
+			return nil, errors.New(message) // Some error
+		}
+	}
+	return &Lv{lv}, nil
 }
-
 
 func CreateLV(
-    vg C.vg_t, pool string, volId string, size uint64, fs string,
+	vg C.vg_t, pool string, volId string, size uint64, fs string,
 ) (*Lv, error) {
-    goPool :=C.CString(pool)
-    defer C.free(unsafe.Pointer(goPool))
-    goVolId :=C.CString(volId)
-    defer C.free(unsafe.Pointer(goVolId))
-    params := C.lvm_lv_params_create_thin(
-        vg, goPool, goVolId, C.uint64_t(size),
-    )
-    if params == nil {
-        return nil, errors.New(C.GoString(C.lvm_errmsg(libHandle)))
-    }
-    clv := C.lvm_lv_create(params)
-    if clv == nil {
-        return nil, errors.New(C.GoString(C.lvm_errmsg(libHandle)))
-    }
-    lv := Lv{clv}
-    path := lv.Path()
-    cmd := exec.Command("mkfs", "-t", fs, path)
-    err := cmd.Run()
-    if err != nil {
-        return nil, err
-    }
-    return &lv, nil
+	goPool := C.CString(pool)
+	defer C.free(unsafe.Pointer(goPool))
+	goVolId := C.CString(volId)
+	defer C.free(unsafe.Pointer(goVolId))
+	params := C.lvm_lv_params_create_thin(
+		vg, goPool, goVolId, C.uint64_t(size),
+	)
+	if params == nil {
+		return nil, errors.New(C.GoString(C.lvm_errmsg(libHandle)))
+	}
+	clv := C.lvm_lv_create(params)
+	if clv == nil {
+		return nil, errors.New(C.GoString(C.lvm_errmsg(libHandle)))
+	}
+	lv := Lv{clv}
+	path := lv.Path()
+	cmd := exec.Command("mkfs", "-t", fs, path)
+	err := cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+	return &lv, nil
 }
 
-
-func (lv Lv) Name() (string) {
-    return C.GoString(C.lvm_lv_get_name(lv.lv))
+func (lv Lv) Name() string {
+	return C.GoString(C.lvm_lv_get_name(lv.lv))
 }
 
-
-func (lv Lv) Path() (string) {
-    pathString := C.CString("lv_path");
-    defer C.free(unsafe.Pointer(pathString))
-    property := C.lvm_lv_get_property(lv.lv, pathString)
-    return C.GoString(C.getString(property))
+func (lv Lv) Path() string {
+	pathString := C.CString("lv_path")
+	defer C.free(unsafe.Pointer(pathString))
+	property := C.lvm_lv_get_property(lv.lv, pathString)
+	return C.GoString(C.getString(property))
 }
-
 
 func EnsureDevice(
-    vgName string, pool string, volId string, size uint64, fs string,
+	vgName string, pool string, volId string, size uint64, fs string,
 ) (*Lv, error, bool) {
-    vg, err := GetVG(vgName)
-    if err != nil {
-        return nil, err, false
-    }
-    lv, err := GetLV(vg, volId)
-    if err != nil {
-        return nil, err, false
-    }
-    if lv != nil {
-        return lv, nil, false
-    }
-    lv, err = CreateLV(vg, pool, volId, size, fs)
-    if err != nil {
-        return nil, err, false
-    }
-    return lv, nil, true
+	vg, err := GetVG(vgName)
+	if err != nil {
+		return nil, err, false
+	}
+	lv, err := GetLV(vg, volId)
+	if err != nil {
+		return nil, err, false
+	}
+	if lv != nil {
+		return lv, nil, false
+	}
+	lv, err = CreateLV(vg, pool, volId, size, fs)
+	if err != nil {
+		return nil, err, false
+	}
+	return lv, nil, true
 }
